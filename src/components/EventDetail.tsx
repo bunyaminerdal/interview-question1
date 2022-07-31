d28a952275488bf632b6a39a5ab48ca7346c4de0
leonidas131 <leonidas131@hotmail.com>
2022-07-31 13:02:00 +0300

    image fullscreen modal added

--- src/components/EventDetail.tsx
+++ src/components/EventDetail.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useAppSelector } from '../store'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import ReactAudioPlayer from 'react-audio-player'
+import { ReactComponent as Fullscreen } from '../icons/Fullscreen.svg'
+import { ReactComponent as FullscreenExit } from '../icons/Fullscreen-exit.svg'
 
 const selectionEnum = {
     details: 'details',
@@ -11,6 +13,7 @@ const selectionEnum = {
 const EventDetail = () => {
     const [selection, setSelection] = useState(selectionEnum.details)
     const { selectedData } = useAppSelector(state => state.events)
+
     useEffect(() => {
         setSelection(selectionEnum.details)
     }, [selectedData])
@@ -36,30 +39,46 @@ const EventDetail = () => {
                     </div>
                         : null}
                     {selection === selectionEnum.location ? <div>
-                        {selectedData.location ? <MapContainer center={[selectedData?.location?.latitude, selectedData?.location?.longitude]} zoom={13} scrollWheelZoom={false} >
-                            <TileLayer
-                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            />
-                            <Marker position={[selectedData?.location?.latitude, selectedData?.location?.longitude]}>
-                                <Popup>
-                                    {selectedData.id}
-                                </Popup>
-                            </Marker>
-                        </MapContainer> : <div>There is no map information</div>}
-
+                        {selectedData.location ?
+                            <MapContainer center={[selectedData?.location?.latitude, selectedData?.location?.longitude]} zoom={13} scrollWheelZoom={false} >
+                                <TileLayer
+                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <Marker position={[selectedData?.location?.latitude, selectedData?.location?.longitude]}>
+                                    <Popup>
+                                        {selectedData.id}
+                                    </Popup>
+                                </Marker>
+                            </MapContainer> : <div>There is no map information</div>}
                     </div> : null}
                     {selection === selectionEnum.media ? <div>
-                        {selectedData?.media?.map(media => media.type === "image" ?
-                            <img src={media.url} alt="" />
+                        {selectedData?.media?.map((media, index) => media.type === "image" ? <div>
+
+                            <img className='scaled-image' key={index} src={media.url} alt="" />
+                            <Fullscreen className='fullscreen-icon-container ' fill='white' data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" />
+                            <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div className="modal-dialog modal-dialog-centered">
+                                    <div className="modal-content">
+                                        <div className='modal-body'>
+                                            <img className='fullscreen-image' key={index} src={media.url} alt="" />
+                                            <FullscreenExit className='fullscreen-icon-container' fill='white' type="button" data-bs-dismiss="modal" />
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                             :
-                            <ReactAudioPlayer
+                            <ReactAudioPlayer key={index}
                                 src={media.url}
                                 controls
                             />)}
+
                     </div> : null}
                 </div>
             </div>
+
         </div>
     )
 }
